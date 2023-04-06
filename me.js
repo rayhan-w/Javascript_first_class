3e489a353d095040d8109e5b85a2373fc8d68842
Sahreza <128999717+rayhan-w@users.noreply.github.com>
2023-04-06 13:23:32 -0700

    2nd_Class
    here clear about the concept of function, object , event

--- me.js
+++ me.js
@@ -1,7 +1,7 @@
 
 console.log("Rayhan Khan"); 
 
-document.write("Rayhan Khan"); 
+//document.write("Rayhan Khan\n"); 
 
 //window.alert("Rayhan Khan"); 
 
@@ -16,4 +16,32 @@ let b=5, c;
 //a=14; 
 c= a%b; 
 
-console.log("total number of c is",c); 
+
+
+let x= rayhan(4,4); 
+
+
+function rayhan(a,b){
+    return a+b; 
+}
+
+  //document.write(x); 
+
+let C= celcius(98.5);
+function celcius(f){
+            return (f-32)*5/9;
+}
+
+document.write("celcius scale is",C);
+
+const person ={
+    firstname: "rayhan", 
+    lastname: "Hayder", 
+    age: 23, 
+
+
+};
+
+console.log(person); 
+
+console.log(person.age); 
