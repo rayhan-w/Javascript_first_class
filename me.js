a28baecd284ae49193ca06647b81fc6bd0cf4a13
Sahreza <128999717+rayhan-w@users.noreply.github.com>
2023-04-06 00:48:00 -0700

    first_commit

---
+++ me.js
@@ -0,0 +1,19 @@
+
+console.log("Rayhan Khan"); 
+
+document.write("Rayhan Khan"); 
+
+//window.alert("Rayhan Khan"); 
+
+// var , let , const ; 
+
+// variable (variable name = varaible value / x=1) num1 num_1 
+
+const a=10; 
+
+let b=5, c; 
+
+//a=14; 
+c= a%b; 
+
+console.log("total number of c is",c); 
